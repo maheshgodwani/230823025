91fc5bfae691ea017bebbd9cd30cf3c4039c844d
maheshgodwani <mhsmca5723@gmail.com>
2024-03-14 09:19:08 +0530

    Props add

--- story component/src/story.js
+++ story component/src/story.js
@@ -1,15 +1,15 @@
 import './Story.css';
-function Story() {
+function Story(props) {
     return(
         <div className="story-container">
       <img
         className="story-image"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GBuo5fELJCr7nJ293kFWoKG4-0Lz0-M4Ww&usqp=CAU"
+        src={props.simg}
         alt="Story Image"
       />
       <div className="story-footer">
-        <img className="imag1" src="https://media.licdn.com/dms/image/D4D03AQGVl11fuDsJWQ/profile-displayphoto-shrink_200_200/0/1709208456970?e=2147483647&v=beta&t=6BzKtR0oFBrggQAI47Hf3w1LxHNWYsI8SpkV2Q8ixjw" alt="User Profile" />
-        <h3>MHS Patel</h3>
+        <img className="imag1" src={props.pimg} alt="User Profile" />
+        <h3>{props.name}</h3>
       </div>
     </div>
     );
