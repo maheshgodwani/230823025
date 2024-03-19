bfdf4cb36030c074d4a3353342207900cd49d6fb
maheshgodwani <mhsmca5723@gmail.com>
2024-03-20 02:27:17 +0530

    Scrollbar Added

--- src/story.js
+++ src/story.js
@@ -2,15 +2,15 @@ import './Story.css';
 
 function Story(props) {
     return(
-
     <>
       <div className="story-container">
-      <img src={props.storyImage} alt="Story" className="story-image" />
-      <div className="story-footer">
-        <img src={props.profileImage} alt="Profile" className="profile-image" />
-        <h3>{props.storyTitle}</h3>
+        <img src={props.storyImage} alt="Story" className="story-image" />
+        
+        <div className="story-footer">
+          <img src={props.profileImage} alt="Profile" className="profile-image" />
+          <h3>{props.storyTitle}</h3>
+        </div>
       </div>
-    </div>
     </>
     );
 }
